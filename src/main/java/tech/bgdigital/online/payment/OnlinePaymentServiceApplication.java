d0f1a653944c5861f034d61c79895f11023f9fe2
papesambandour <papesambandour@hotmail.com>
2021-11-18 15:14:32 +0000

    add

---
+++ src/main/java/tech/bgdigital/online/payment/OnlinePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.bgdigital.online.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/bgdigital/online/payment/OnlinePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bgdigital.online.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

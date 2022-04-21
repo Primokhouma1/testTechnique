ee09cc632b5856f5bf167045c5f7b16beb580fd0
papesambandour <papesambandour@hotmail.com>
2022-04-21 12:23:22 +0000

    fixed bug paiement

--- src/main/java/tech/bgdigital/online/payment/models/repository/AccountStatementRepository.java
+++ src/main/java/tech/bgdigital/online/payment/models/repository/AccountStatementRepository.java
@@ -3,12 +3,13 @@ package tech.bgdigital.online.payment.models.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import tech.bgdigital.online.payment.models.entity.AccountStatement;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AccountStatementRepository extends JpaRepository<AccountStatement, Integer> {
+public interface AccountStatementRepository extends JpaRepository<AccountStatement, Integer>/*, JpaSpecificationExecutor<AccountStatement> */{
     Optional<AccountStatement> findById(Integer id);
     Page<AccountStatement> findAllByStateIsNot(String state, Pageable pageable);
     List<AccountStatement> findAllByStateNot(String state);
